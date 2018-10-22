cea46dca431bd8d39b12d8a28f395f9798f3444c
naoya0082 <6ds3f7@gmail.com>
2018-10-22 11:03:16 +0900

    Initial Commit

---
+++ CountApp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountApp2
+//
+//  Created by 高橋直也 on 2018/10/22.
+//  Copyright © 2018 Takahashi Naoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
