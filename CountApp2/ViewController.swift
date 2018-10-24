9d1c8b6245d5c4d101a4eb234e2bbca6dcf1945e
naoya0082 <6ds3f7@gmail.com>
2018-10-24 23:43:04 +0900

    countApp

--- CountApp2/ViewController.swift
+++ CountApp2/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var number = 0
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        label.text = "0"
     }
 
-
+    @IBAction func ACButton(_ sender: Any) {
+        number = 0
+        label.text = String(number)
+    }
+    
+    @IBAction func plusButton(_ sender: Any) {
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+        number = number - 1
+        label.text = String(number)
+    }
 }
-
